68e0b29f82af0b3e0dcc152ab761e420ef01c348
NathanNoye <32617224+NathanNoye@users.noreply.github.com>
2018-07-21 01:02:26 -0300

    Added PHP support / title tag / meta description
    When filling out the project information - if "PHP" is set as the primary language - the BP code will generate a basic PHP structure including header, footer, functions, and cnn files for includes.
    Title tags now get filled by using the Project Title
    Meta Description uses the Description given to be used by search engines.
    less likely to crash
    more dope

--- site-starter.bat
+++ site-starter.bat
@@ -1,5 +1,7 @@
 CLS
 NETSH wlan show interfaces | FINDSTR /c:"Signal" && SET online=ONLINE || SET online=OFFLINE
+SET primaryLanguage=html
+SET useAgile=N
 CLS
 
 TITLE Agile2 Site generator
@@ -12,8 +14,7 @@ ECHO.
 
 SET /p location="Path to project folder: "
 SET /p author="Author: "
-SET /p company="Company: "
-SET /p companyURL="Company website: "
+SET /p authorURL="Author website: "
 SET /p projectTitle="Project title: "
 SET /p primaryLanguage="Primary Programming Language: "
 SET /p description="Description: "
@@ -29,11 +30,10 @@ MD includes
 REM Creating the JSON file
 ECHO ^{ >> info.json
 ECHO    "Author"^:"%author%"^, >> info.json
-ECHO    "Company"^:"%company%"^, >> info.json
-ECHO    "Company Website"^:"%companyURL%"^, >> info.json
+ECHO    "Author Website"^:"%authorURL%"^, >> info.json
 ECHO    "Title"^:"%projectTitle%"^, >> info.json
 ECHO    "Primary Language"^:"%primaryLanguage%"^, >> info.json
-ECHO    "Date"^:"%date%"^, >> info.json
+ECHO    "Date Created"^:"%date%"^, >> info.json
 ECHO    "Description"^:"%description%"^ >> info.json
 ECHO ^} >> info.json
 
@@ -75,25 +75,95 @@ IF /I %useAgile%==Y (
 )
 
 
+IF /I %primaryLanguage%==PHP (
+    GOTO PHPCODEGEN
+) ELSE (
+    GOTO HTMLCODEGEN
+)
+
+:PHPCODEGEN
+REM Creating the PHP files
+REM Header
+ECHO ^<!DOCTYPE html^> >> includes/header.php
+ECHO ^<html lang="en"^> >> includes/header.php
+ECHO    ^<head^> >> includes/header.php
+ECHO        ^<title^>%projectTitle%^</title^> >>includes/header.php
+ECHO        ^<meta charset="utf-8"^> >> includes/header.php
+ECHO        ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^> >> includes/header.php
+ECHO        ^<meta name="description" content="%description%"^> >> includes/header.php
+ECHO        ^<link rel="stylesheet" type="text/css" href=%cssLink%^> >> includes/header.php
+ECHO    ^</head^> >> includes/header.php
+ECHO    ^<body^> >> includes/header.php
+
+REM Main
+ECHO ^<^?php >> index.php
+ECHO    include ^'includes^/cnn.php^'^; >> index.php
+ECHO    include ^'includes^/functions.php^'^; >> index.php
+ECHO    include ^'includes^/header.php^'^; >> index.php
+ECHO ^?^> >> index.php
+ECHO. >> index.php
+ECHO ^<^?php >> index.php
+ECHO    include ^'includes^/footer.php^'^; >> index.php
+ECHO ^?^> >> index.php
+
+REM Footer
+ECHO        ^<footer^>^<a href="https://%authorURL%"^>Site developed by %author%^<^/a^>^</footer^> >> includes/footer.php
+ECHO        ^<!-- Put JS scripts here --^> >> includes/footer.php
+ECHO        ^<script src=%scriptLink%^>^</script^> >> includes/footer.php
+ECHO    ^</body^> >> includes/footer.php
+ECHO ^</html^> >> includes/footer.php
+
+REM Connection File
+ECHO ^<^?php >> includes/cnn.php
+ECHO function makeConnection^(^$databaseName^)^{ >> includes/cnn.php
+ECHO    ^$server = "localhost"; >> includes/cnn.php
+ECHO    ^$user  = ""; >> includes/cnn.php
+ECHO    ^$password  = ""; >> includes/cnn.php
+ECHO. >> includes/cnn.php
+ECHO    $conn = new mysqli($server, $user, $password, $databaseName); >> includes/cnn.php
+ECHO. >> includes/cnn.php
+ECHO    if ($conn -^> connect_error){ >> includes/cnn.php
+ECHO        echo "Error: Failed to make a db connection. Here's why: "; >> includes/cnn.php
+ECHO        echo "Error Num: " . $conn -^> connect_errno . "\n"; >> includes/cnn.php
+ECHO. >> includes/cnn.php
+ECHO        die("Connection Failed " . $conn -^> connect_error); ^/^/Log this on the server >> includes/cnn.php
+ECHO    ^} else ^{ >> includes/cnn.php
+ECHO        return $conn; >> includes/cnn.php
+ECHO    ^} >> includes/cnn.php
+ECHO ^} >> includes/cnn.php
+ECHO ^?^> >> includes/cnn.php
+
+REM Functions
+ECHO ^<^?php >> includes/functions.php
+ECHO. >> includes/functions.php
+ECHO ^?^> >> includes/functions.php
+GOTO END
 
 
+
+:HTMLCODEGEN
 REM Creating the HTML file
 ECHO ^<!DOCTYPE html^> >> index.html
 ECHO ^<html lang="en"^> >> index.html
 ECHO    ^<head^> >> index.html
-ECHO        ^<title^>^</title^> >>index.html
+ECHO        ^<title^>%projectTitle%^</title^> >>index.html
 ECHO        ^<meta charset="utf-8"^> >> index.html
 ECHO        ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^> >> index.html
+ECHO        ^<meta name="description" content="%description%"^> >> index.html
 ECHO        ^<link rel="stylesheet" type="text/css" href=%cssLink%^> >> index.html
 ECHO    ^</head^> >> index.html
 ECHO    ^<body^> >> index.html
 ECHO        ^<header^>^</header^> >> index.html
 ECHO        ^<section^>^</section^> >> index.html
-ECHO        ^<footer^>^</footer^> >> index.html
+ECHO        ^<footer^>^<a href="https://%authorURL%"^>Site developed by %author%^<^/a^>^</footer^> >> index.html
 ECHO        ^<!-- Put JS scripts here --^> >> index.html
 ECHO        ^<script src=%scriptLink%^>^</script^> >> index.html
 ECHO    ^</body^> >> index.html
 ECHO ^</html^> >> index.html
+GOTO END
 
+
+
+:END
 REM Opening the folder to get started
 START %location%
